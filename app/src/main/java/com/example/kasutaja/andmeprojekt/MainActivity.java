78adbb83bdbb530accf07520be20bc7aef5ebcc8
TheAndry <andrykore@gmail.com>
2018-02-13 22:14:19 +0200

    Sign in files.

--- app/src/main/java/com/example/kasutaja/andmeprojekt/MainActivity.java
+++ app/src/main/java/com/example/kasutaja/andmeprojekt/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -14,12 +15,28 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.kasutaja.andmeprojekt.customViews.MainListObject;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity { //implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener
+
+    private LinearLayout Prof_Section;
+    private Button SignOut;
+    private SignInButton SignIn;
+    private TextView Name,Email;
+    private GoogleApiClient googleApiClient;
+    private static final int REQ_CODE = 9001;
 
     int arv;
     boolean isEditable = false;
@@ -28,7 +45,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
+        /*
+        Prof_Section = (LinearLayout)findViewById(R.id.prof_section);
+        SignOut = (Button)findViewById(R.id.bn_logout);
+        SignIn = (SignInButton)findViewById(R.id.bn_login);
+        Name = (TextView)findViewById(R.id.name);
+        Email = (TextView)findViewById(R.id.email);
+        SignIn.setOnClickListener(this);
+        SignOut.setOnClickListener(this);
+        Prof_Section.setVisibility(View.GONE);
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, this).addApi(Auth.GOOGLE_SIGN_IN_API, signInOptions).build();
+        */
 
         retrieveDataFromPhone();
 
@@ -43,6 +74,7 @@ public class MainActivity extends AppCompatActivity {
 
         //Tarv.setText(String.valueOf(arv));
 
+
         FloatingActionButton fb = findViewById(R.id.dataFloatingActionButton);
 
         fb.setOnClickListener(new View.OnClickListener() {
@@ -51,6 +83,9 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), CreateData.class));
             }
         });
+
+
+
     }
 
     protected void retrieveDataFromPhone() {
@@ -104,5 +139,69 @@ public class MainActivity extends AppCompatActivity {
 
         ll.addView(menuObject);
     }
+    /*
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bn_login:
+                signIn();
+                break;
+            case R.id.bn_logout:
+                signOut();
+                break;
+        }
+    }
 
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    private void signIn(){
+        Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(intent, REQ_CODE);
+    }
+
+    private void signOut(){
+        Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                updateUI(false);
+            }
+        });
+    }
+
+    private void handleResult(GoogleSignInResult result){
+        if(result.isSuccess()){
+            GoogleSignInAccount account = result.getSignInAccount();
+            String name = account.getDisplayName();
+            String email = account.getEmail();
+            Name.setText(name);
+            Email.setText(email);
+            updateUI(true);
+        } else {
+            updateUI(false);
+        }
+    }
+
+    private void updateUI(boolean isLogin){
+        if(isLogin) {
+            Prof_Section.setVisibility(View.VISIBLE);
+            SignIn.setVisibility(View.GONE);
+            setContentView(R.layout.activity_main);
+        } else {
+            Prof_Section.setVisibility(View.GONE);
+            SignIn.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQ_CODE){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleResult(result);
+        }
+    }*/
 }
